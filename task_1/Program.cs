763056f14176e77a129616ce4ef65884747c09cf
Khurshed Muradov <hurshed@gmail.com>
2024-07-18 05:34:14 +0500

    added c#seminar_hw_1:task_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -4,3 +4,16 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+System.Console.WriteLine("Введите первое число: ");
+int firstDigit = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите первое число: ");
+int secondDigit = int.Parse(Console.ReadLine());
+if (firstDigit > secondDigit)
+{
+    System.Console.WriteLine("Max = " + firstDigit);
+}
+else
+{
+    System.Console.WriteLine("Max = " + secondDigit);
+}
