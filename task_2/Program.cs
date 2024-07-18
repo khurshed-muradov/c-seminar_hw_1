a17bb6283d2d16584fbd0e5cdda31bc08b361680
Khurshed Muradov <hurshed@gmail.com>
2024-07-18 05:47:54 +0500

    added c#seminar_hw_1:task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -3,3 +3,24 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+System.Console.WriteLine("Введите первое число: ");
+int firstDigit = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите первое число: ");
+int secondDigit = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите третье число: ");
+int thirdDigit = int.Parse(Console.ReadLine()!);
+int max = firstDigit;
+if (secondDigit > max)
+{
+    max = secondDigit;
+
+}
+if (thirdDigit > max)
+{
+    max = thirdDigit;
+}
+else
+{
+    max = thirdDigit;
+}
+System.Console.WriteLine("-> " + max);
