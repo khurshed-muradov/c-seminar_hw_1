6b3bcee699e073a489c447e3447c11c641f18755
Khurshed Muradov <hurshed@gmail.com>
2024-07-18 06:40:41 +0500

    added c#seminar_hw_1:task_4

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -3,3 +3,10 @@
 // числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+int digit = int.Parse(Console.ReadLine());
+int startEvenNumber = 2;
+while (startEvenNumber <= digit)
+{
+    System.Console.Write(startEvenNumber + ", ");
+    startEvenNumber += 2;
+}
