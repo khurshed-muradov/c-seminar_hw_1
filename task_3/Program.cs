b35b90d5fc567e7e765c1a71d1613e1632460d2d
Khurshed Muradov <hurshed@gmail.com>
2024-07-18 05:58:07 +0500

    added c#seminar_hw_1:task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -4,3 +4,14 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+System.Console.WriteLine("Введите число: ");
+int digit = int.Parse(Console.ReadLine()!);
+int evenNumber = digit % 2;
+if (evenNumber == 0)
+{
+    System.Console.WriteLine("-> да");
+}
+else
+{
+    System.Console.WriteLine("-> нет");
+}
